02898fd84f0a55ada62f82aa1dcee47bb3bd9d33
Никола Цветановски <nikolacvetanovski1996@outlook.com>
2026-06-29 19:35:31 +0200

    Add health check endpoint for uptime monitoring

---
+++ Turbo_Wheels/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using Turbo_Wheels.Filters;
+
+namespace Turbo_Wheels.Controllers
+{
+    [AllowAnonymous]
+    public class HealthController : Controller
+    {
+        [NoCache]
+        [AcceptVerbs(HttpVerbs.Get | HttpVerbs.Head)]
+        public ActionResult Index()
+        {
+            return Content("Healthy", "text/plain");
+        }
+    }
+}
